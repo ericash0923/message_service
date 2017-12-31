72a71d9e53157c2120493386a8dee09b278d8141
Eric Ash <eric@gaglani.com>
2017-12-31 00:59:58 -0500

    first commit with TDD and BDD setup

---
+++ spec/models/notification_spec.rb
@@ -0,0 +1,12 @@
+# spec/models/notification.rb
+
+require 'rails_helper'
+
+RSpec.describe Notification, type: :model do
+  describe 'creation' do
+    it 'can be created' do
+      notification = FactoryBot.build_stubbed(:notification)
+      expect(notification).to be_valid
+    end
+  end
+end
