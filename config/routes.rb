91719c7867a9f963c8fe571269103ada6b635fed
Eric Ash <eric@gaglani.com>
2018-01-01 19:15:01 -0500

    integrated error status test and data integrity validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :notifications
+  resources :notifications, :defaults => { :format => 'json' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/requests/notification_spec.rb
+++ spec/requests/notification_spec.rb
@@ -22,5 +22,23 @@ RSpec.describe Notification, type: :request do
     expect(response).to have_http_status(:created)
   end
 
+  it 'renders an error status if the notification was not created' do 
+    header = {
+      "ACCEPT" => "application/json"
+    }
+
+    post "/notifications",
+    :params =>
+    { :notification =>
+     {
+        :phone => "5555555555",
+        :body => "My Message"
+      }
+    }, :headers => headers 
+
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
 end
 
