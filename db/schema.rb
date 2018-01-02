efe515e56d76e14015ab2176756b57b91e3decad
Eric Ash <eric@gaglani.com>
2018-01-01 23:28:13 -0500

    implemented client model and some refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231053321) do
+ActiveRecord::Schema.define(version: 20180102041857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "source_app"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notifications", force: :cascade do |t|
     t.string "phone"
---
+++ spec/factories/clients.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :client do
+    source_app "my_app"
+    api_key "0CoKxKDZ8wBQiG1x7rDqDwtt"
+  end
+end
